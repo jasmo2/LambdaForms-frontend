b095dfea13541a6aacd4223d2f8e71358af1be27
jaime <jaime.molina@mattersupply.co>
2020-01-23 12:56:00 -0500

    lambda

--- src/layouts/App.tsx
+++ src/layouts/App.tsx
@@ -5,32 +5,46 @@ import Helmet from 'react-helmet'
 import './index.css'
 import theme from '~/theme/base'
 
+import ApolloClient, { InMemoryCache } from 'apollo-boost'
+import { ApolloProvider } from '@apollo/react-hooks'
+
+const createApolloClient = () => {
+  const client = new ApolloClient({
+    uri: `/.netlify/functions/main`,
+    cache: new InMemoryCache()
+  })
+
+  return client
+}
+
 const App = props => {
   return (
-    <ThemeProvider theme={theme}>
-      <Helmet>
-        <title>Material Gatsby</title>
-        <meta charSet="utf-8" />
-        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, viewport-fit=cover"
-        />
-        <meta
-          name="description"
-          content="Material UI template to integrate directly with Gatsby"
-        />
-        <link
-          href="https://fonts.googleapis.com/css?family=Roboto+Mono|Roboto+Slab|Roboto:300,400,500,700"
-          rel="stylesheet"
-        />
-        <script
-          type="text/javascript"
-          src="//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js"
-          async></script>
-      </Helmet>
-      {props.children}
-    </ThemeProvider>
+    <ApolloProvider client={createApolloClient()}>
+      <ThemeProvider theme={theme}>
+        <Helmet>
+          <title>Material Gatsby</title>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, viewport-fit=cover"
+          />
+          <meta
+            name="description"
+            content="Material UI template to integrate directly with Gatsby"
+          />
+          <link
+            href="https://fonts.googleapis.com/css?family=Roboto+Mono|Roboto+Slab|Roboto:300,400,500,700"
+            rel="stylesheet"
+          />
+          <script
+            type="text/javascript"
+            src="//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js"
+            async></script>
+        </Helmet>
+        {props.children}
+      </ThemeProvider>
+    </ApolloProvider>
   )
 }
 interface DefaultLayoutProps extends React.HTMLProps<HTMLDivElement> {
